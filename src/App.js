ab11a801fc4ea5391ce41a04a8206b3d8a237614
Estelle <estellepieragnolo@gmail.com>
2019-03-25 11:21:39 +0100

    fetch data  from api

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      error: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://private-cc77e-aff.apiary-mock.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ items: data.items }))
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          error: true
+        });
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="App" />
     );
   }
 }
