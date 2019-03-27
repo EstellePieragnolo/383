eff3f87b4355d55c2822a5c0a53cfe62a1f02e92
Estelle <estellepieragnolo@gmail.com>
2019-03-27 12:25:27 +0100

    add pinterest like layout

--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
 import PostItem from './PostItem';
 import './_Post.scss';
+import StackGrid from "react-stack-grid";
 
 class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { width: 0 };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
 
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth });
+    }
+
+    setColumnWidth = () => {
+        const size = this.state.width;
+        if (size <= 735) {
+            return '100%'
+        }
+        else if (size > 735 && size <= 972) {
+            return '50%'
+        }
+        else return '33.33%'
+    }
+    something = () => {
+        this.grid.updateLayout();
+    };
     render() {
+
         return (
             <div className="container">
-                {
-                    this.props.items.map(items => {
-                        return (
+                <StackGrid
+                    monitorImagesLoaded
+                    columnWidth={this.setColumnWidth()}
+                    gutterWidth={30}
+                    gutterHeight={30}
+                    gridRef={grid => this.grid = grid}
+                >
+                    {
+                        this.props.items.map(items => {
+                            return (
 
-                            <PostItem key={items.item_id}
-                                item_id={items.item_id}
-                                item_data={items.item_data}
-                                service_name={items.service_name}
-                                item_published={items.item_published} />
+                                <PostItem key={items.item_id}
+                                    item_id={items.item_id}
+                                    item_data={items.item_data}
+                                    service_name={items.service_name}
+                                    item_published={items.item_published} />
 
-                        );
+                            );
 
-                    })
-                }
+                        })
+                    }
+
+                    {/* < i aria-hidden='true' />
+                    <i aria-hidden='true' /> */}
+                </StackGrid>
             </div>
         );
     }
